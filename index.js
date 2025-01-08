913ecec258c3085b04408e881476c694ec227cfa
Saj1d-prompt <sajidulislam2225@gmail.com>
2025-01-08 21:02:52 +0600

    Displayed Meals in Card

--- index.js
+++ index.js
@@ -12,13 +12,26 @@ function process(data){
     oldContent.textContent = "";
     for(var i=0;i<meals.length;i++){
         var newDiv = document.createElement("div");
+        // newDiv.innerHTML = `
+        //    <img src = "${meals[i].strMealThumb}"> <br>
+        //    Meal ID: ${meals[i].idMeal} <br>
+        //     Meal Name: ${meals[i].strMeal} <br>
+        //     Meal Area: ${meals[i].strArea} <br>
+        //     Cooking Instruction: <br>
+        //     ${meals[i].strInstructions}
+        // `;
+
         newDiv.innerHTML = `
-           <img src = "${meals[i].strMealThumb}"> <br>
-           Meal ID: ${meals[i].idMeal} <br>
-            Meal Name: ${meals[i].strMeal} <br>
-            Meal Area: ${meals[i].strArea} <br>
-            Cooking Instruction: <br>
-            ${meals[i].strInstructions}
+        <div class="card" style="width: 18rem;">
+            <img src="${meals[i].strMealThumb}" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">Meal Name: ${meals[i].strMeal}</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">Meal ID: ${meals[i].idMeal}</h6>
+                <p class="card-text">Meal Area: ${meals[i].strArea}</p>
+                <p class="card-text">Cooking Instruction</p>
+                <p class="card-text">${meals[i].strInstructions}</p>
+            </div>
+        </div>
         `;
         oldContent.appendChild(newDiv);
     }
