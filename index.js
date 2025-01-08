ff9420509b27d3c72cdeb34dee29bc3e977c1b73
Saj1d-prompt <sajidulislam2225@gmail.com>
2025-01-08 21:55:43 +0600

    Showed All items on Show All button click

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function search(){
     var meal = document.getElementById("mealSearch").value;
     var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`;
@@ -10,11 +11,11 @@ function process(data){
     console.log(meals);
     var oldContent = document.getElementById("display");
     oldContent.textContent = "";
-    var newDiv = document.createElement("div");
+    
     var counter = 0;
     for(var i=0;i<meals.length;i++){
         if(counter<5){
-            
+            var newDiv = document.createElement("div");
             newDiv.innerHTML = `
                 <div class="card" style="width: 18rem;">
                     <img src="${meals[i].strMealThumb}" class="card-img-top">
@@ -26,8 +27,6 @@ function process(data){
                         <p class="card-text">${meals[i].strInstructions}</p>
                     </div>
                 </div>
-
-            
             `;
             oldContent.appendChild(newDiv);
 
@@ -40,3 +39,32 @@ function process(data){
     }
 }
 
+function showAll(){
+    var meal = document.getElementById("mealSearch").value;
+    var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`;
+
+    fetch(url).then(res => res.json()).then(data => show(data));
+}
+
+function show(data){
+    var meals = data.meals;
+    var oldContent = document.getElementById("show");
+    
+    for(var i=5;i<meals.length;i++){
+        var newDiv = document.createElement("div");
+        newDiv.innerHTML = `
+            <div class="card" style="width: 18rem;">
+                <img src="${meals[i].strMealThumb}" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Meal Name: ${meals[i].strMeal}</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">Meal ID: ${meals[i].idMeal}</h6>
+                    <p class="card-text">Meal Area: ${meals[i].strArea}</p>
+                    <p class="card-text">Cooking Instruction</p>
+                    <p class="card-text">${meals[i].strInstructions}</p>
+                </div>
+            </div>            
+            `;
+            oldContent.appendChild(newDiv);
+    }
+
+}
