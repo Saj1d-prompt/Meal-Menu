62bd5503916be045d68c5dd09a4a58cc10b9f235
Saj1d-prompt <sajidulislam2225@gmail.com>
2025-01-08 20:46:19 +0600

    tried to display image on UI

--- index.js
+++ index.js
@@ -2,5 +2,19 @@ function search(){
     var meal = document.getElementById("mealSearch").value;
     var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`;
 
-    fetch(url).then(res => res.json()).then(data => console.log(data));
+    fetch(url).then(res => res.json()).then(data => process(data));
+}
+
+function process(data){
+    var meals = data.meals;
+    console.log(meals);
+    var oldContent = document.getElementById("display");
+    oldContent.textContent = "";
+    for(var i=0;i<meals.length;i++){
+        var newDiv = document.createElement("div");
+        newDiv.innerHTML = `
+           <img src = "${meals[i].strMealThumb}"> 
+        `;
+        oldContent.appendChild(newDiv);
+    }
 }
