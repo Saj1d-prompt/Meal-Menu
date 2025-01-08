3b2d019b2beb020f7d7570a59d6d2e65f5fa571a
Saj1d-prompt <sajidulislam2225@gmail.com>
2025-01-08 21:24:06 +0600

    trying to make it responsive

--- index.js
+++ index.js
@@ -22,16 +22,20 @@ function process(data){
         // `;
 
         newDiv.innerHTML = `
-        <div class="card" style="width: 18rem;">
-            <img src="${meals[i].strMealThumb}" class="card-img-top">
-            <div class="card-body">
-                <h5 class="card-title">Meal Name: ${meals[i].strMeal}</h5>
-                <h6 class="card-subtitle mb-2 text-body-secondary">Meal ID: ${meals[i].idMeal}</h6>
-                <p class="card-text">Meal Area: ${meals[i].strArea}</p>
-                <p class="card-text">Cooking Instruction</p>
-                <p class="card-text">${meals[i].strInstructions}</p>
-            </div>
+        <div class="row">
+                <div class="card col col-lg-4 col-md-6" style="width: 18rem;">
+                    <img src="${meals[i].strMealThumb}" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Meal Name: ${meals[i].strMeal}</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Meal ID: ${meals[i].idMeal}</h6>
+                        <p class="card-text">Meal Area: ${meals[i].strArea}</p>
+                        <p class="card-text">Cooking Instruction</p>
+                        <p class="card-text">${meals[i].strInstructions}</p>
+                    </div>
+                </div>
+        
         </div>
+        
         `;
         oldContent.appendChild(newDiv);
     }
